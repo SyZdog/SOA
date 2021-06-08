4ceafebff2d80c6d969e870cf333c23dfd5eb6ab
SyZdog <45461241+SyZdog@users.noreply.github.com>
2021-06-08 22:12:37 +0800

    nothing

--- sp04-orderservice/src/main/java/cn/tedu/sp04/Sp04OrderserviceApplication.java
+++ sp04-orderservice/src/main/java/cn/tedu/sp04/Sp04OrderserviceApplication.java
@@ -2,7 +2,8 @@ package cn.tedu.sp04;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Sp04OrderserviceApplication {
 
---
+++ sp05-eureka/src/test/java/cn/tedu/sp05/Sp05EurakaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tedu.sp05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp05EurakaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
