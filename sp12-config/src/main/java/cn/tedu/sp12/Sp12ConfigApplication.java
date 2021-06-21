dc581ae067b52dd2b6dcc8335f47a0423b41cebf
SyZdog <45461241+SyZdog@users.noreply.github.com>
2021-06-21 19:19:00 +0800

    update

--- sp12-config/src/main/java/cn/tedu/sp12/Sp12ConfigApplication.java
+++ sp12-config/src/main/java/cn/tedu/sp12/Sp12ConfigApplication.java
@@ -2,8 +2,12 @@ package cn.tedu.sp12;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableDiscoveryClient
 @SpringBootApplication
+@EnableConfigServer
 public class Sp12ConfigApplication {
 
 	public static void main(String[] args) {
