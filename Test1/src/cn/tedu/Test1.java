07ed335b6caa686464d58d6b19ec3c254733b02d
SyZdog <45461241+SyZdog@users.noreply.github.com>
2021-06-21 10:38:50 +0800

    新建一个测试项目

--- Test1/src/cn/tedu/Test1.java
+++
@@ -1,6 +0,0 @@
-package cn.tedu;
-
-public class Test1 {
-	//1111111111
-	//2222222222
-}
---
+++ sp12-config/src/main/java/cn/tedu/sp12/Sp12ConfigApplication.java
@@ -0,0 +1,13 @@
+package cn.tedu.sp12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp12ConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp12ConfigApplication.class, args);
+	}
+
+}
